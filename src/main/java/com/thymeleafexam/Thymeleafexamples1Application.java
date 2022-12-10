a0c10374ba8bf6318b1fc643bbc909da211bbc84
ARUN KUMAR <arunkrgs2008@gmail.com>
2022-12-10 19:09:25 +0530

    Developed Register form using ThymeLeaf

---
+++ src/main/java/com/thymeleafexam/Thymeleafexamples1Application.java
@@ -0,0 +1,13 @@
+package com.thymeleafexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Thymeleafexamples1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Thymeleafexamples1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/thymeleafexam/Thymeleafexamples1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thymeleafexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Thymeleafexamples1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
